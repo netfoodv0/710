962d5b0743d36aeacc81f1acb7a6502f47d4f9fc
SEU-NOME <SEU-EMAIL@exemplo.com>
2025-08-26 00:17:27 -0300

    fix: configuração condicional do Vite para funcionar local e no Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/' : './',
   plugins: [
     react(),
   ],
